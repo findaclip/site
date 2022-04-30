d2df8ca6dfe482bc0da8af50003b7d67837f174f
rmenai <rami.menai@outlook.com>
2022-04-30 23:20:21 +0300

    Add multi-platform favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,29 @@ export default defineNuxtConfig({
   srcDir: "src/",
   buildModules: ["@pinia/nuxt", "@vueuse/nuxt", "nuxt-windicss"],
   modules: ["@nuxtjs/color-mode"],
+  head: {
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/site.webmanifest" },
+    ],
+  },
+
   css: ["assets/css/base.scss"],
   colorMode: {
     classSuffix: "",
