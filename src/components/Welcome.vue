096d0f3c300aca9efd4b99f40f68fab7a65abc4d
rmenai <rami.menai@outlook.com>
2022-05-01 12:56:38 +0300

    Add under developpment svg

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,3 +1,12 @@
 <template>
-  <NuxtWelcome />
+  <Head>
+    <Title> Find a Clip - Home </Title>
+  </Head>
+  <div class="flex h-screen items-center justify-center">
+    <img
+      class="border-16 md:border-64 h-full w-11/12 border-transparent object-center"
+      src="https://www.redimec.eu/images/svg-incollato-412902x200.svg"
+      alt="Under construction"
+    />
+  </div>
 </template>
